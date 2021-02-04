9103f4a2c5e4897e9659edf1d55a58d8a9e5a1e3
Denys Smirnov <dendi239@gmail.com>
2021-02-04 19:48:26 +0200

    Update README.md, make function produces movie by given id.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 from justwatch import JustWatch
 
-
 API_TOKEN = os.environ['API_TOKEN']
 
 logging.basicConfig(level=logging.INFO)
@@ -25,12 +24,14 @@ providers = {provider['id']: provider for provider in jw.get_providers()}
 
 @dp.message_handler(commands=['start', 'help'])
 async def show_help(message: types.Message) -> None:
-    help_message = "Это *Cinemabot*. " \
-                   "Бот который умеет искать фильмы и/или сериалы для просмотра.\n\n" \
-                   "Для простого поиска просто введите запрос. " \
-                   "Далее можете либо посмотреть первый найденный фильм, либо выбрать из результатов поиска.\n\n" \
-                   "/start, /help покажут это сообщение снова\n" \
-                   "/todo покажет сообщение с текущим тудулистом"
+    help_message = \
+        "Это *Cinemabot*. " \
+        "Бот который умеет искать фильмы и/или сериалы для просмотра.\n\n" \
+        "Для простого поиска просто введите запрос. " \
+        "Далее можете либо посмотреть первый найденный фильм, либо выбрать из результатов поиска.\n\n" \
+        "/start, /help покажут это сообщение снова\n" \
+        "/todo покажет сообщение с текущим тудулистом\n" \
+        "/schedule `N` `query` выполнит поиск через `N` секунд\n"
 
     await bot.send_message(message.chat.id, help_message, parse_mode=types.ParseMode.MARKDOWN)
 
@@ -48,14 +49,25 @@ async def show_todo(message: types.Message) -> None:
     await bot.send_message(message.chat.id, todo_message, parse_mode=types.ParseMode.MARKDOWN)
 
 
-@dp.message_handler()
-async def search_for_film(message: types.Message) -> None:
-    async for film in search_for_item(message.text):
+@dp.message_handler(commands=['schedule'])
+async def schedule(message: types.Message) -> None:
+    command, duration, query = message.text.split(maxsplit=3)
+    await asyncio.sleep(int(duration))
+    await send_results(query, message)
+
+
+async def send_results(query: str, message: types.Message, full_results: bool = True) -> None:
+    async for film in search_for_item(query):
         keyboard = WrappedInlineKeyboardMarkup()
-        keyboard.add(
-            *(types.InlineKeyboardButton(offer.cinema, url=offer.url) for offer in film.offers),
-            types.InlineKeyboardButton('more', callback_data=f'list:{message.text}'),
-        )
+        if full_results:
+            keyboard.add(
+                *(types.InlineKeyboardButton(offer.cinema, url=offer.url) for offer in film.offers),
+                types.InlineKeyboardButton('more', callback_data=f'list:{query}'),
+            )
+        else:
+            keyboard.add(
+                *(types.InlineKeyboardButton(offer.cinema, url=offer.url) for offer in film.offers),
+            )
 
         await bot.send_photo(message.chat.id, film.get_poster_url(), format_description(film),
                              parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
@@ -63,6 +75,11 @@ async def search_for_film(message: types.Message) -> None:
     else:
         await message.reply(f'Ничего не найдено по запросу "{message.text}"',
                             reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler()
+async def search_for_film(message: types.Message) -> None:
+    await send_results(message.text, message)
 
 
 @dataclasses.dataclass
@@ -111,7 +128,7 @@ class Movie(BaseMovie):
             try:
                 cinema_link = CinemaLink(offer_json)
                 offers[cinema_link.cinema] = cinema_link
-            except IndexError:
+            except KeyError:
                 pass
 
         self.offers = list(offers.values())
@@ -127,7 +144,7 @@ def format_description(movie: Movie) -> str:
 
     return f"<b>{name_line}</b>\n" \
            f"\n" \
-           f"{movie.short_description}"
+           f"{movie.short_description}\n"
 
 
 async def base_search_for_item(query: str) -> tp.AsyncIterable[BaseMovie]:
@@ -141,7 +158,7 @@ async def base_search_for_item(query: str) -> tp.AsyncIterable[BaseMovie]:
     for result_json in results:
         try:
             yield BaseMovie(result_json)
-        except IndexError:
+        except KeyError:
             pass
 
 
@@ -152,12 +169,12 @@ async def search_for_item(query: str) -> tp.AsyncIterable[Movie]:
 
         try:
             yield Movie(film_json)
-        except IndexError:
+        except KeyError:
             pass
 
 
 class WrappedInlineKeyboardMarkup(types.InlineKeyboardMarkup):
-    def __init__(self, symbols_limit: int = 25, count_limit: int = 3) -> None:
+    def __init__(self, symbols_limit: int = 23, count_limit: int = 3) -> None:
         self.symbols_limit = symbols_limit
         super().__init__(row_width=count_limit)
 
@@ -187,7 +204,7 @@ async def movie_by_id(callback_data: types.CallbackQuery) -> None:
 
     try:
         film = Movie(film_json)
-    except IndexError:
+    except KeyError:
         return
 
     keyboard = WrappedInlineKeyboardMarkup()
