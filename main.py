1e416f046e4c52e103b7b9b4031839101d1cdef7
Denys Smirnov <dendi239@gmail.com>
2021-02-10 03:25:29 +0200

    Remove path from setwebhook

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def main():
         webhook_port = int(os.environ['WEBHOOK_PORT'])
 
         async def on_startup(dp: Dispatcher) -> None:
-            await bot.set_webhook(f'{webhook_host}/webhook')
+            await bot.set_webhook(f'{webhook_host}')
 
         async def on_shutdown(dp: Dispatcher) -> None:
             await bot.delete_webhook()
